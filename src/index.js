cb7b3c512da7d6d44e1c65cbc160307ba12eeffa
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-02-01 18:29:32 -0500

    require in proper files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+require('dotenv').config();
 
+//db setup
+const User = require('./models/user');
+require('./db/mongoose');
+//require in external routes
+const userRoutes = require('./routes/users');
+
+//express server setup
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+//use routes required in
+app.use(userRoutes);
+
+//express server setup
 app.listen(port, () => {
   console.log(`server is up on port: ${port}`);
 });
