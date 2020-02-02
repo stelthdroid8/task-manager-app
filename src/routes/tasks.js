163e1da084eac039c58be535c4985ae0f2a22bd0
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-02-02 15:36:47 -0500

    add in handlers for getting all and getting by id

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,6 +6,31 @@ const Task = require('../models/task');
 
 router = express.Router();
 
+router.get('/tasks', (req, res) => {
+  Task.find({})
+    .then(tasks => {
+      res.status(200).send(tasks);
+    })
+    .catch(error => {
+      res.status(500).send();
+    });
+});
+
+router.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  // console.log(User.findById(_id));
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch(error => {
+      res.status(500).send();
+    });
+});
+
 router.post('/tasks', (req, res) => {
   const task = new Task(req.body);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,6 +6,31 @@ const User = require('../models/user');
 
 router = express.Router();
 
+router.get('/users', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.status(200).send(users);
+    })
+    .catch(error => {
+      res.status(500).send();
+    });
+});
+
+router.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  // console.log(User.findById(_id));
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(error => {
+      res.status(500).send();
+    });
+});
+
 router.post('/users', (req, res) => {
   const user = new User(req.body);
 
