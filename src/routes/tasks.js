a81ee57461ed889248deb8635046d7bb40b494fd
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-02-03 16:53:20 -0500

    refactor to aync functions

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,42 +6,39 @@ const Task = require('../models/task');
 
 router = express.Router();
 
-router.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.status(200).send(tasks);
-    })
-    .catch(error => {
-      res.status(500).send();
-    });
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
-router.get('/tasks/:id', (req, res) => {
+router.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
-  // console.log(User.findById(_id));
-  Task.findById(_id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch(error => {
-      res.status(500).send();
-    });
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
-router.post('/tasks', (req, res) => {
+router.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch(error => {
-      res.status(400).send(error);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,42 +6,38 @@ const User = require('../models/user');
 
 router = express.Router();
 
-router.get('/users', (req, res) => {
-  User.find({})
-    .then(users => {
-      res.status(200).send(users);
-    })
-    .catch(error => {
-      res.status(500).send();
-    });
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
-  // console.log(User.findById(_id));
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch(error => {
-      res.status(500).send();
-    });
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
-router.post('/users', (req, res) => {
+router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(error => {
-      res.status(400).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
