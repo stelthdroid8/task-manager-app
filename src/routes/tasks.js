1a92b6fa185a17444e722661754289f87a5666eb
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-02-03 19:27:27 -0500

    remove debugging log

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -44,12 +44,14 @@ router.patch('/tasks/:id', async (req, res) => {
 
   const _id = req.params.id;
 
-  console.log(_id);
   try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const task = await Task.findById(_id);
+    updates.forEach(update => (task[update] = req.body[update]));
+    await task.save();
+    // const task = await Task.findByIdAndUpdate(_id, req.body, {
+    //   new: true,
+    //   runValidators: true
+    // });
 
     if (!task) {
       return res.status(404).send();
