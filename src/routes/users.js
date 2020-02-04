68b2663004ebd8da12db4ad67e62e5fe1ba1566e
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-02-04 16:46:52 -0500

    add login route

--- src/routes/users.js
+++ src/routes/users.js
@@ -86,4 +86,16 @@ router.post('/users', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 module.exports = router;
