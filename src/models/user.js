bc5303ff5a7a87e2cc329b386dfacf3f7631d3ce
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-02-04 17:25:09 -0500

    include token array on user model, generateAuth functionality

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,7 +40,15 @@ const userSchema = new mongoose.Schema({
         throw new Error('Age must be a positive number greater than 0');
       }
     }
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -56,6 +65,17 @@ userSchema.statics.findByCredentials = async (email, password) => {
   }
 
   return user;
+};
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWTKEY);
+
+  user.tokens = user.tokens.concat({ token });
+
+  await user.save();
+
+  return token;
 };
 
 // middleware to hash passwords
